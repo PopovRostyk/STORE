80440a1e5ffa06cfdef15daf9b7d6d8f5da0d830
dfghjm <popov@ucu.edu.ua>
2017-11-14 00:56:23 +0200

    ~

--- src/main/java/com/Main.java
+++ src/main/java/com/Main.java
@@ -2,12 +2,14 @@ package com;
 
 public class Main {
     public static void main(String[] args) {
+        int priceNew = 45;
+        int serialNewnumber = 1111;
         Inventory phoneStore = new Inventory();
-        Phone iphone = new Phone(1111, 45, new Spec(Type.SENSOR,
+        Phone iphone = new Phone(serialNewnumber, priceNew, new Spec(Type.SENSOR,
                 "10", "false", "USA"));
-        Phone phone = new Phone(1111, 45, new Spec(Type.SENSOR,
+        Phone phone = new Phone(serialNewnumber, priceNew, new Spec(Type.SENSOR,
                 "10", "false", "UA"));
-        Phone samsung = new Phone(1111, 45, new Spec(Type.SENSOR,
+        Phone samsung = new Phone(serialNewnumber, priceNew, new Spec(Type.SENSOR,
                 "s6", "false", "GB"));
         phoneStore.addPhone(iphone.getSerialNumber(), iphone.getPrice(),
                 iphone.getSpecs());
